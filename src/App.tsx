cef7ea94472b94c091b5b6e9144e0d2afcb56b8a
Siwat Anandechsak <bright.siwat@hotmail.com>
2023-10-11 11:41:06 +0700

    style :nail_care: : responsive

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,28 @@ import Register from './pages/Register'
 import CreatePost from './pages/CreatePost'
 import Post from './pages/Post'
 import Edit from './pages/Edit'
+import GuardedRoute from './guard/GuardedRoute'
+import { useAuth } from './providers/AuthProvider'
 
 function App() {
+  const { isLoggedIn } = useAuth()
   return (
     <>
       <Navbar />
       <div className=" px-16  font-Kanit">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/create" element={<CreatePost />} />
           <Route path="/posts/:id" element={<Post />} />
-          <Route path="/posts/:id/edit" element={<Edit />} />
+          {/* Can't go to this path if a user is not log in  */}
+          <Route element={<GuardedRoute isRouteAccessible={isLoggedIn} redirectRoute="/login" />}>
+            <Route path="/create" element={<CreatePost />} />
+            <Route path="/posts/:id/edit" element={<Edit />} />
+          </Route>
+          {/* Can't go to this path if a user is log in  */}
+          <Route element={<GuardedRoute isRouteAccessible={!isLoggedIn} redirectRoute="/" />}>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
         </Routes>
       </div>
     </>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -27,7 +27,7 @@ const Main = () => {
         </Link>
       </div>
 
-      <div className="grid grid-cols-3 gap-8 mt-11">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-11">
         {posts &&
           currentPosts.map((post) => {
             return <CardPost key={post.id} post={post} />
